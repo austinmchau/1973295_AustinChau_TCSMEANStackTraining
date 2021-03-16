c08523a612606a06eddeda34f95def5c2c5d89a6
Austin C <austin.chau@tcs.com>
2021-03-15 18:07:54 -0700

    Use a prototype, added jsdocs

--- TeamBudgetPlanner/budgetplanner.js
+++ TeamBudgetPlanner/budgetplanner.js
@@ -1,10 +1,26 @@
 /* Variables */
+/* Declaring few global variables for the sessionStorage and storage key */
 
 var storageKey = "budgetEntries"
-var getStorage = () => {return sessionStorage}
+var getStorage = () => { return sessionStorage }
+
+/**
+ * A prototype representing a budget entry.
+ * @param {string} clientName 
+ * @param {string} projectName 
+ * @param {number} budget 
+ */
+function BudgetEntry(clientName, projectName, budget) {
+    this.clientName = clientName;
+    this.projectName = projectName;
+    this.budget = budget
+}
 
 /* Adding Entries */
 
+/**
+ * callback function for when the add budget form is being submitted
+ */
 function onBudgetFormSubmit() {
     var entry = getBudgetFormData()
     if (entry !== null) {
@@ -13,22 +29,36 @@ function onBudgetFormSubmit() {
     }
 }
 
+/**
+ * Responsible for getting input from the add budget form. 
+ * Performs validation on whether any field is empty. Raise alert if so.
+ * @returns {BudgetEntry|null} A budget entry.
+ */
 function getBudgetFormData() {
-    var entry = {}
-    entry.clientName = document.getElementById("clientName").value;
-    entry.projectName = document.getElementById("projectName").value;
-    entry.budget = Number(document.getElementById("budget").value);
+    var clientName = document.getElementById("clientName").value;
+    var projectName = document.getElementById("projectName").value;
+    var budget = document.getElementById("budget").value;
 
-    if (!entry.clientName || !entry.projectName || !budget) {
+    if (clientName == "" || projectName == "" || budget == "") {
         alert("All fields must be filled.")
         return null;
     }
+    budget = Number(budget)
+    if (isNaN(budget)) {
+        alert("Budget has to be a valid number.")
+        return null;
+    }
 
+    var entry = new BudgetEntry(clientName, projectName, budget)
+    console.log(entry)
     return entry
 }
 
 /* Rendering Entries */
 
+/**
+ * Generates the budget view table when called. 
+ */
 function generateBudgetTable() {
     var entries = getBudgetEntries()
     var totalBudget = 0;
@@ -36,28 +66,34 @@ function generateBudgetTable() {
     var table = document.getElementById("budgetTable")
     var body = table.getElementsByTagName("tbody")[0]
 
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
+        if (!entry) { return }
+
         var row = document.createElement("tr");
 
         [entry.clientName, entry.projectName, entry.budget].forEach(item => {
-            var cell = document.createElement("td")
+            var cell = document.createElement("td");
             cell.appendChild(document.createTextNode(
-                typeof item == 'number' ? item.toLocaleString('en-US', {style: "currency", currency: "USD"}) : item
-            ))
-            row.appendChild(cell)
-        })
+                typeof item == 'number' ? item.toLocaleString('en-US', { style: "currency", currency: "USD" }) : item
+            ));
+            row.appendChild(cell);
+        });
 
-        body.appendChild(row)
+        body.appendChild(row);
 
-        totalBudget += entry.budget
+        totalBudget += entry.budget;
     })
 
     var total = document.getElementById("budgetTotal")
-    total.innerText = totalBudget.toLocaleString('en-US', {style: "currency", currency: "USD"})
+    total.innerText = totalBudget.toLocaleString('en-US', { style: "currency", currency: "USD" })
 }
 
 /* Entries Storage */
 
+/**
+ * Get all budget entries from storage.
+ * @returns {BudgetEntry[]} entries
+ */
 function getBudgetEntries() {
     var entries
     var storedEntries = getStorage().getItem(storageKey)
@@ -79,6 +115,10 @@ function getBudgetEntries() {
     return entires
 }
 
+/**
+ * Add an entry to the sessionStorage.
+ * @param {BudgetEntry} entry 
+ */
 function addBudgetEntry(entry) {
     var entries = getBudgetEntries()
     entries.push(entry)
@@ -88,6 +128,9 @@ function addBudgetEntry(entry) {
     )
 }
 
+/**
+ * Reset the sessionStorage for budget entries
+ */
 function resetBudgetEntry() {
     getStorage().removeItem(storageKey)
 }
