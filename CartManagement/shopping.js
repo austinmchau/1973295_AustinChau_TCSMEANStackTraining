62d72886dba3bf552127fd586297383b32344086
Austin C <austin.chau@tcs.com>
2021-03-22 17:25:03 -0700

    added checkout page and relevant code. added plus minus quantity button for inventory

--- CartManagement/shopping.js
+++ CartManagement/shopping.js
@@ -1,3 +1,7 @@
+/* Prototypes */
+/**
+ * A mock db of all possible inventory for purchase.
+ */
 var inventory = [
     {
         "id": "1",
@@ -8,32 +12,126 @@ var inventory = [
     {
         "id": "2",
         "name": "Desktop",
-        "price": 800
+        "price": 800,
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/b/b6/Computer.tower.750pix.jpg"
     },
     {
         "id": "3",
         "name": "Monitor",
-        "price": 250
+        "price": 250,
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/c/cf/ASUS_MK241H_20080707.jpg"
     },
     {
         "id": "4",
         "name": "Keyboard",
-        "price": 80
+        "price": 80,
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/2007_09_30_de_Apple-Tastatur.jpg/1024px-2007_09_30_de_Apple-Tastatur.jpg"
     },
     {
         "id": "5",
-        "name": "Smart Phone",
-        "price": 1200
+        "name": "Smartphone",
+        "price": 1200,
+        "imageUrl": "https://images.pexels.com/photos/4387770/pexels-photo-4387770.jpeg?cs=srgb&dl=pexels-stanley-ng-4387770.jpg&fm=jpg"
     },
     {
         "id": "6",
         "name": "Mouse",
-        "price": 30
+        "price": 30,
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/c/c5/Red_computer_mouse.jpg"
     }
 ];
-function updateCart(options) {
-    console.debug("updating cart: ", options.itemId);
+/* Utilities */
+/**
+ * A util function for formatting a number into US currency.
+ * @param value a number that to be converted to currency format.
+ * @returns a formatted string representing the monetary value.
+ */
+function formatAsCurrency(value) {
+    return Intl.NumberFormat('en-US', {
+        style: "currency",
+        currency: "USD"
+    }).format(value);
 }
+function inventoryFromId(id) {
+    var filtered = inventory.filter(function (item) { return item.id == id; });
+    if (!(filtered === null || filtered === void 0 ? void 0 : filtered.length)) {
+        return null;
+    }
+    return filtered[0];
+}
+/* Cart Storage */
+var Cart = /** @class */ (function () {
+    function Cart() {
+        this.storage = function () { return localStorage; };
+        this.storageKey = "shoppingCart";
+    }
+    Cart.getInstance = function () {
+        if (!Cart.instance) {
+            Cart.instance = new Cart();
+        }
+        return Cart.instance;
+    };
+    Cart.prototype.clear = function () { this.storage().removeItem(this.storageKey); };
+    Cart.prototype.read = function () {
+        var store = this.storage().getItem(this.storageKey);
+        if (store === null) {
+            return {};
+        }
+        else {
+            try {
+                return JSON.parse(store);
+            }
+            catch (error) {
+                this.clear();
+                console.error(error);
+                return {};
+            }
+        }
+    };
+    Cart.prototype.write = function (partialCart) {
+        var cart = this.read();
+        console.debug("write cart: ", cart);
+        console.debug("partial cart: ", partialCart);
+        for (var id in partialCart) {
+            if (!(id in cart)) {
+                cart[id] = 0;
+            }
+            cart[id] = Math.max(0, cart[id] + partialCart[id]);
+        }
+        console.debug("write cart after: ", cart);
+        try {
+            var json = JSON.stringify(cart);
+            this.storage().setItem(this.storageKey, json);
+        }
+        catch (error) {
+            this.clear();
+            console.error(error);
+        }
+    };
+    Cart.prototype.updateCart = function (itemId, quantity) {
+        var _a;
+        console.debug("updating cart: ", itemId, quantity);
+        this.write((_a = {}, _a[itemId] = quantity, _a));
+    };
+    Cart.prototype.description = function () {
+        var cart = this.read();
+        return Object.keys(cart).map(function (k) {
+            return {
+                item: inventoryFromId(k),
+                quantity: cart[k]
+            };
+        });
+    };
+    Cart.prototype.quantityFromId = function (id) {
+        var cart = this.read();
+        if (!(id in cart)) {
+            return null;
+        }
+        return cart[id];
+    };
+    return Cart;
+}());
+/* Listing Page */
 function updateListing() {
     var listingArea = document.getElementById("listingArea");
     var listingTemplate = document.getElementById("listingTemplate").content;
@@ -48,11 +146,29 @@ function updateListing() {
         console.debug("card: ", card);
         var nameLabel = card.querySelectorAll("h2")[0];
         var priceLabel = card.querySelectorAll("p")[0];
-        var quantityButton = card.querySelectorAll("input")[0];
+        var quantitySubButton = card.querySelectorAll("button")[0];
+        var quantityLabel = card.getElementById("quantityLabel");
+        var quantityAddButton = card.querySelectorAll("button")[1];
         var imagePreview = card.querySelectorAll("img")[0];
-        nameLabel.textContent = "Name: " + entry.name;
-        priceLabel.textContent = "Price: " + entry.price;
-        quantityButton.onclick = (function (ev) { return updateCart({ itemId: entry.id }); });
+        nameLabel.textContent = "" + entry.name;
+        priceLabel.textContent = "Price: " + formatAsCurrency(entry.price);
+        var labelId = "quantityLabel-" + entry.id;
+        quantityLabel.id = labelId;
+        quantityLabel.innerText = "" + Cart.getInstance().quantityFromId(entry.id);
+        quantitySubButton.onclick = (function (ev) {
+            Cart.getInstance().updateCart(entry.id, -1);
+            var label = document.getElementById(labelId);
+            if (label !== null) {
+                label.innerText = "" + Cart.getInstance().quantityFromId(entry.id);
+            }
+        });
+        quantityAddButton.onclick = (function (ev) {
+            Cart.getInstance().updateCart(entry.id, 1);
+            var label = document.getElementById(labelId);
+            if (label !== null) {
+                label.innerText = "" + Cart.getInstance().quantityFromId(entry.id);
+            }
+        });
         if (!entry.imageUrl) {
             imagePreview.remove();
         }
@@ -61,4 +177,38 @@ function updateListing() {
         }
         listingArea.appendChild(card);
     });
+}
+/* Checkout */
+function updateCheckoutTable() {
+    console.debug("Checkout table: ", Cart.getInstance().description());
+    var checkoutTable = document.getElementById("checkoutTable");
+    if (!checkoutTable) {
+        console.error("Empty checkoutTable");
+        return;
+    }
+    var tbody = checkoutTable.getElementsByTagName("tbody")[0];
+    var itemTemplate = checkoutTable.getElementsByTagName("template")[0].content;
+    var totalTemplate = checkoutTable.getElementsByTagName("template")[1].content;
+    console.debug("template: ", itemTemplate);
+    console.debug("tbody: ", tbody);
+    while (tbody === null || tbody === void 0 ? void 0 : tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+    var totalPrice = 0;
+    Cart.getInstance().description().forEach(function (_a) {
+        var item = _a.item, quantity = _a.quantity;
+        var row = itemTemplate.cloneNode(true);
+        var nameLabel = row.querySelectorAll("td")[0];
+        var quantityLabel = row.querySelectorAll("td")[1];
+        var priceLabel = row.querySelectorAll("td")[2];
+        var itemPrice = item.price * quantity;
+        nameLabel.textContent = "" + item.name;
+        quantityLabel.textContent = "" + quantity;
+        priceLabel.textContent = "" + formatAsCurrency(itemPrice);
+        totalPrice += itemPrice;
+        tbody.appendChild(row);
+    });
+    var totalRow = totalTemplate.cloneNode(true);
+    totalRow.querySelectorAll("th")[1].textContent = "" + formatAsCurrency(totalPrice);
+    tbody.appendChild(totalRow);
 }
--- CartManagement/shopping.ts
+++ CartManagement/shopping.ts
@@ -1,3 +1,8 @@
+/* Prototypes */
+
+/**
+ * A mock db of all possible inventory for purchase.
+ */
 const inventory: InventoryItem[] = [
     {
         "id": "1",
@@ -8,31 +13,38 @@ const inventory: InventoryItem[] = [
     {
         "id": "2",
         "name": "Desktop",
-        "price": 800
+        "price": 800,
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/b/b6/Computer.tower.750pix.jpg"
     },
     {
         "id": "3",
         "name": "Monitor",
-        "price": 250
+        "price": 250,
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/c/cf/ASUS_MK241H_20080707.jpg"
     },
     {
         "id": "4",
         "name": "Keyboard",
-        "price": 80
+        "price": 80,
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/2007_09_30_de_Apple-Tastatur.jpg/1024px-2007_09_30_de_Apple-Tastatur.jpg"
     },
     {
         "id": "5",
-        "name": "Smart Phone",
-        "price": 1200
+        "name": "Smartphone",
+        "price": 1200,
+        "imageUrl": "https://images.pexels.com/photos/4387770/pexels-photo-4387770.jpeg?cs=srgb&dl=pexels-stanley-ng-4387770.jpg&fm=jpg"
     },
     {
         "id": "6",
         "name": "Mouse",
-        "price": 30
+        "price": 30,
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/c/c5/Red_computer_mouse.jpg"
     }
 ]
 
-
+/**
+ * Interface for a specific item.
+ */
 interface InventoryItem {
     id: string,
     name: string,
@@ -40,9 +52,100 @@ interface InventoryItem {
     imageUrl?: string,
 }
 
-function updateCart(options: { itemId: string }) {
-    console.debug("updating cart: ", options.itemId);
+
+/* Utilities */
+
+/**
+ * A util function for formatting a number into US currency.
+ * @param value a number that to be converted to currency format.
+ * @returns a formatted string representing the monetary value.
+ */
+function formatAsCurrency(value: number): string {
+    return Intl.NumberFormat('en-US', {
+        style: "currency",
+        currency: "USD",
+    }).format(value);
 }
+
+function inventoryFromId(id: string): InventoryItem | null {
+    const filtered = inventory.filter(item => item.id == id);
+    if (!filtered?.length) { return null; }
+    return filtered[0];
+}
+
+/* Cart Storage */
+
+class Cart {
+
+    private static instance: Cart;
+    private constructor() { }
+
+    public static getInstance(): Cart {
+        if (!Cart.instance) {
+            Cart.instance = new Cart();
+        }
+        return Cart.instance;
+    }
+
+    protected storage = () => localStorage;
+    protected readonly storageKey = "shoppingCart";
+
+    protected clear() { this.storage().removeItem(this.storageKey); }
+    protected read(): { [id: string]: number } {
+        const store = this.storage().getItem(this.storageKey);
+        if (store === null) {
+            return {};
+        } else {
+            try {
+                return JSON.parse(store);
+            } catch (error) {
+                this.clear();
+                console.error(error);
+                return {};
+            }
+        }
+    }
+    protected write(partialCart: { [id: string]: number }) {
+        const cart = this.read();
+        console.debug("write cart: ", cart);
+        console.debug("partial cart: ", partialCart);
+        for (const id in partialCart) {
+            if (!(id in cart)) { cart[id] = 0; }
+            cart[id] = Math.max(0, cart[id] + partialCart[id]);
+        }
+        console.debug("write cart after: ", cart);
+        try {
+            const json = JSON.stringify(cart);
+            this.storage().setItem(this.storageKey, json);
+        } catch (error) {
+            this.clear();
+            console.error(error);
+        }
+    }
+
+    updateCart(itemId: string, quantity: number) {
+        console.debug("updating cart: ", itemId, quantity);
+        this.write({ [itemId]: quantity });
+    }
+
+    description(): { item: InventoryItem, quantity: number }[] {
+        const cart = this.read();
+        return Object.keys(cart).map(k => {
+            return {
+                item: inventoryFromId(k),
+                quantity: cart[k]
+            };
+        })
+    }
+
+    quantityFromId(id: string): number | null {
+        const cart = this.read();
+        if (!(id in cart)) { return null; }
+        return cart[id];
+    }
+}
+
+/* Listing Page */
 
 function updateListing() {
     let listingArea = document.getElementById("listingArea");
@@ -61,11 +164,29 @@ function updateListing() {
         console.debug("card: ", card);
         let nameLabel = card.querySelectorAll("h2")[0];
         let priceLabel = card.querySelectorAll("p")[0];
-        let quantityButton = card.querySelectorAll("input")[0];
+        let quantitySubButton = card.querySelectorAll("button")[0];
+        let quantityLabel = card.getElementById("quantityLabel");
+        let quantityAddButton = card.querySelectorAll("button")[1];
         let imagePreview = card.querySelectorAll("img")[0];
-        nameLabel.textContent = `Name: ${entry.name}`;
-        priceLabel.textContent = `Price: ${entry.price}`;
-        quantityButton.onclick = (ev => updateCart({ itemId: entry.id }));
+        nameLabel.textContent = `${entry.name}`;
+        priceLabel.textContent = `Price: ${formatAsCurrency(entry.price)}`;
+        const labelId = `quantityLabel-${entry.id}`;
+        quantityLabel.id = labelId;
+        quantityLabel.innerText = `${Cart.getInstance().quantityFromId(entry.id)}`;
+        quantitySubButton.onclick = (ev => {
+            Cart.getInstance().updateCart(entry.id, -1);
+            let label = document.getElementById(labelId)
+            if (label !== null) {
+                label.innerText = `${Cart.getInstance().quantityFromId(entry.id)}`;
+            }
+        });
+        quantityAddButton.onclick = (ev => {
+            Cart.getInstance().updateCart(entry.id, 1);
+            let label = document.getElementById(labelId)
+            if (label !== null) {
+                label.innerText = `${Cart.getInstance().quantityFromId(entry.id)}`;
+            }
+        });
         if (!entry.imageUrl) {
             imagePreview.remove();
         } else {
@@ -73,4 +194,42 @@ function updateListing() {
         }
         listingArea.appendChild(card);
     })
+}
+
+/* Checkout */
+
+function updateCheckoutTable() {
+    console.debug("Checkout table: ", Cart.getInstance().description());
+
+    let checkoutTable = document.getElementById("checkoutTable");
+    if (!checkoutTable) { console.error("Empty checkoutTable"); return; }
+    let tbody = checkoutTable.getElementsByTagName("tbody")[0];
+    let itemTemplate = (checkoutTable.getElementsByTagName("template")[0] as HTMLTemplateElement).content;
+    let totalTemplate = (checkoutTable.getElementsByTagName("template")[1] as HTMLTemplateElement).content;
+
+    console.debug("template: ", itemTemplate);
+    console.debug("tbody: ", tbody);
+
+    while (tbody?.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+
+    let totalPrice = 0;
+    Cart.getInstance().description().forEach(({ item, quantity }) => {
+        let row = itemTemplate.cloneNode(true) as DocumentFragment;
+        let nameLabel = row.querySelectorAll("td")[0];
+        let quantityLabel = row.querySelectorAll("td")[1];
+        let priceLabel = row.querySelectorAll("td")[2];
+
+        let itemPrice = item.price * quantity;
+        nameLabel.textContent = `${item.name}`;
+        quantityLabel.textContent = `${quantity}`;
+        priceLabel.textContent = `${formatAsCurrency(itemPrice)}`;
+
+        totalPrice += itemPrice;
+        tbody.appendChild(row);
+    })
+    let totalRow = totalTemplate.cloneNode(true) as DocumentFragment;
+    totalRow.querySelectorAll("th")[1].textContent = `${formatAsCurrency(totalPrice)}`;
+    tbody.appendChild(totalRow);
 }
