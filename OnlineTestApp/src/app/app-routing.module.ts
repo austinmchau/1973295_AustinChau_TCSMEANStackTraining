9fdf11f639bc72825c46e3a01269fa87807538e6
Austin C <austin.chau@tcs.com>
2021-03-27 11:22:25 -0700

    added bootstrap and rudimentary quiz components

--- OnlineTestApp/src/app/app-routing.module.ts
+++ OnlineTestApp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizComponent } from './quiz-components/quiz/quiz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "quiz", component: QuizComponent },
+  { path: "", redirectTo: "quiz", pathMatch: "full" },  // TODO: remove redirect once splash screen is ready
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- OnlineTestApp/src/app/app.module.ts
+++ OnlineTestApp/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { QuizQuestionComponent } from './quiz-components/quiz-question/quiz-question.component';
+import { QuizComponent } from './quiz-components/quiz/quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizQuestionComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
