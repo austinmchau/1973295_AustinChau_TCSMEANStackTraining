5250753240c0207627db588749984b255710abea
Austin C <austin.chau@tcs.com>
2021-04-01 17:10:18 -0700

    added toolbar and add-task and task-view components

--- TaskTracker/src/app/app.module.ts
+++ TaskTracker/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { TaskViewComponent } from './components/task-view/task-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    TaskViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+		MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
