46303790413af570e54533b5e97d6ceb5ef36d96
Austin C <austin.chau@tcs.com>
2021-04-05 20:23:49 -0700

    tidying up the code

--- LogRecords/logger.js
+++ LogRecords/logger.js
@@ -1,17 +1,17 @@
-let readline = require("readline");
-
-let getIo = () => readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
 /**
- * 
- * @param {readline.Interface} ioInterface 
- * @param {[string, string][]} questions 
- * @returns {Promise<{[varName: string]: string}>}
+ * Function that asks an array of questions and returns a Promise of user response.
+ * @param {[string, string][]} questions Array of tuple of [the question to ask, variable name for the response.]
+ * @param {readline.ReadLineOptions | undefined} interface optional configuration for the readline.
+ * @returns {Promise<{[varName: string]: string}>} A Promise that returns and object of {[variable name from questions]: response}
  */
-function askQuestions(ioInterface, questions) {
+function askQuestions(questions, interface) {
+
+    const readline = require("readline");
+
+    const ioInterface = readline.createInterface(interface ?? {
+        input: process.stdin,
+        output: process.stdout,
+    });
 
     const prompt = (acc, question, varName) => new Promise((resolve) => {
         ioInterface.question(`${question}: `, (response) => {
@@ -22,18 +22,49 @@ function askQuestions(ioInterface, questions) {
     return questions
         .reduce(
             async (response, [q, v]) => prompt(await response, q, v),
-            Promise.resolve()
+            Promise.resolve({})
         )
         .finally(() => {
             ioInterface.close();
         });
 }
 
+function logToFile(entry) {
 
-askQuestions(getIo(), [
-    ["Enter your first name", "firstName"],
-    ["Enter your last name", "lastName"],
-])
-    .then(results => {
-        console.log(results);
-    });
+}
+
+/**
+ * Function that asks for user info and save to json file.
+ */
+function logUserRecords() {
+
+    const userInput = askQuestions([
+        ["Enter your first name", "firstName"],
+        ["Enter your last name", "lastName"],
+        ["Enter your gender", "gender"],
+        ["Enter your email", "email"],
+    ])
+        .then((result) => {
+            console.log(`Thanks, ${result.firstName}, your input is saved.`);
+            return result;
+        })
+        .catch(error => {
+            console.error(`Sorry, something wrong happened. Please try again. Error: ${error}`);
+        })
+}
+
+/**
+ * Exports
+ */
+
+exports.askQuestions = askQuestions;
+exports.logToFile = logToFile;
+exports.logUserRecords = logUserRecords;
+
+
+/**
+ * Main entry point when called from cli
+ */
+if (typeof require !== 'undefined' && require.main === module) {
+    logUserRecords();
+}
