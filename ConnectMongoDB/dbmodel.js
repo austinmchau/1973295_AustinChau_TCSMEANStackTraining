7fa608d082d90eb9596a5dbf63f17185f49b0842
Austin C <austin.chau@tcs.com>
2021-04-20 02:44:57 -0700

    working app

--- ConnectMongoDB/dbmodel.js
+++ ConnectMongoDB/dbmodel.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, Db } = require("mongodb");
 
 /** * Constants */
 const dbUrl = "mongodb://localhost:27017";
@@ -13,25 +13,35 @@ class CourseEntry {
 		this.description = description;
 		this.amount = amount;
 	}
+	static fromDb({ _id, courseName, description, amount }) {
+		const invalidProp = [_id, courseName, description, amount].find(item => typeof item !== "string");
+		if (invalidProp !== undefined) { throw new TypeError(`Invalid Prop: ${invalidProp}`); }
+
+		return new CourseEntry({ courseId: _id, courseName: courseName, description: description, amount: amount });
+	}
 	toObject() {
 		return Object.entries(this).reduce((acc, [k, v]) => ({ ...acc, [k]: v }), Object.create({}));
 	}
 }
 
 
-async function addCourse(course) {
-
+async function getClient() {
 	const mongoClient = new MongoClient(dbUrl, { useUnifiedTopology: true });
 	const client = await mongoClient.connect().catch(console.error);
+	if (!client) { throw new TypeError(`Empty Client`); }
+	return client;
+}
 
+async function getCollection(client) {
+	const db = client.db("meanstack");
+	return db.collection("CoursePlatform");
+}
+
+async function addCourse(course) {
+	const client = await getClient();
 	try {
-		if (!client) { throw new TypeError(`Empty Client`); }
-
 		const entry = new CourseEntry(course).toObject();
-
-		const db = client.db("meanstack");
-		const collection = db.collection("CoursePlatform");
-
+		const collection = await getCollection(client);
 		const { insertedCount } = await collection.insertOne(entry);
 		console.log(`Inserted ${insertedCount} courses. ${JSON.stringify(entry)}`);
 	}
@@ -44,4 +54,57 @@ async function addCourse(course) {
 	}
 }
 
+async function updateCourse({ courseId, amount }) {
+	const client = await getClient();
+	try {
+		const collection = await getCollection(client);
+		const { modifiedCount } = await collection.updateOne({ _id: courseId }, { $set: { amount: amount } })
+		console.log(`Updated ${modifiedCount} courses. ${JSON.stringify({ courseId, amount })}`);
+	}
+	catch (error) {
+		console.error(error);
+		throw error;
+	}
+	finally {
+		client.close();
+	}
+}
+
+async function deleteCourse({ courseId }) {
+	const client = await getClient();
+	try {
+		const collection = await getCollection(client);
+		const { deletedCount } = await collection.deleteOne({ _id: courseId });
+		console.log(`Deleted ${deletedCount} courses. ${JSON.stringify({ courseId })}`);
+	}
+	catch (error) {
+		console.error(error);
+		throw error;
+	}
+	finally {
+		client.close();
+	}
+}
+
+async function listCourses() {
+	const client = await getClient();
+	try {
+		const collection = await getCollection(client);
+		const docs = await collection.find({}).toArray();
+		const entries = docs.map(doc => CourseEntry.fromDb(doc));
+		console.log(`Retrieved entries. ${JSON.stringify(entries)}`)
+		return entries;
+	}
+	catch (error) {
+		console.error(error);
+		throw error;
+	}
+	finally {
+		client.close();
+	}
+}
+
 exports.addCourse = addCourse;
+exports.updateCourse = updateCourse;
+exports.deleteCourse = deleteCourse;
+exports.listCourses = listCourses;
--- ConnectMongoDB/index.js
+++ ConnectMongoDB/index.js
@@ -39,17 +39,42 @@ app.post("/addCourse/submit", async (req, res) => {
 	console.log("data: ", data);
 	try {
 		await dbModel.addCourse(data);
-		res.status(200).send(
-			redirectHtml("/", 2000) +
-			"Courses added successfully."
-		);
+		res.status(200).send(redirectHtml("/", 2000) + "Courses added successfully.");
+	}
+	catch (error) {
+		res.status(500).send(redirectHtml("/", 2000) + "Course fail to be added.");
+	}
+})
+app.post("/updateCourse/submit", async (req, res) => {
+	const data = req.body;
+	console.log("data: ", data);
+	try {
+		await dbModel.updateCourse(data);
+		res.status(200).send(redirectHtml("/", 2000) + "Courses updated successfully.");
+	}
+	catch (error) {
+		res.status(500).send(redirectHtml("/", 2000) + "Course fail to be updated.");
+	}
+})
+app.post("/deleteCourse/submit", async (req, res) => {
+	const data = req.body;
+	console.log("data: ", data);
+	try {
+		await dbModel.deleteCourse(data);
+		res.status(200).send(redirectHtml("/", 2000) + "Courses deleted successfully.");
+	}
+	catch (error) {
+		res.status(500).send(redirectHtml("/", 2000) + "Course fail to be deleted.");
+	}
+})
+app.get("/fetchCourses/courses", async (req, res) => {
+	try {
+		const entries = await dbModel.listCourses();
+		res.status(200).header({ "Content-Type": "text/json" }).send(entries);
 	}
 	catch (error) {
 		console.error(error);
-		res.status(500).send(
-			redirectHtml("/", 2000) +
-			"Course fail to be added."
-		);
+		res.status(500).header({ "Content-Type": "text/json" }).send([]);
 	}
 })
 
